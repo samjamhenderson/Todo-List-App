dc5c830305ed5ab357db7892c55bfe36ab5c4d04
Samuel Henderson <samuelhenderson@Samuels-Air.cogeco.local>
2023-12-30 17:21:58 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import React, { useRef } from "react";
+import { Todo } from "./Components/Todo";
+
+const App = () => {
+
+
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
